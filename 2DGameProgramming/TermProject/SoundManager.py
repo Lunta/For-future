9a35643b9754ac7816dbb2ab74b2080f5c7ccddf
Sang-gi Lee <tkdqlsek2@gmail.com>
2016-11-08 16:55:33 +0900

    Update - November 08 2016
    Import pygame lib and fixed sound manager.

--- 2DGameProgramming/TermProject/SoundManager.py
+++ 2DGameProgramming/TermProject/SoundManager.py
@@ -1,18 +1,24 @@
-from pico2d import *
+import pygame
 
 
 class SoundManager:
     def __init__(self):
-        self.BGM_Play = load_music('Resource\Sound\BGM\BGM_Play.ogg')
-        self.SE_Punch = load_music('Resource\Sound\Effect\Punch.ogg')
-        self.SE_Kick = load_music('Resource\Sound\Effect\Kick.mp3')
-        self.SE_Kamehameha = load_music('Resource\Sound\Effect\Kamehameha.mp3')
-        self.SE_PowerUp_Sound = load_music('Resource\Sound\Effect\PowerUp_Sound.mp3')
-        self.SE_PowerUp = load_music('Resource\Sound\Effect\PowerUp.mp3')
-        self.SE_PowerUp2 = load_music('Resource\Sound\Effect\PowerUp2.mp3')
-        self.SE_Move = load_music('Resource\Sound\Effect\Move.mp3')
-        self.SE_Boss_Crashed = load_music('Resource\Sound\Effect\Boss_Crashed.ogg')
-        self.SE_Crash_Impact = load_music('Resource\Sound\Effect\Crash_Impact.ogg')
-        self.SE_Meteor_Crashed = load_music('Resource\Sound\Effect\Meteor_Crashed.ogg')
+        pygame.mixer.init()
+        self.BGM_Play = pygame.mixer.Sound('Resource\Sound\BGM\Play.ogg')
+        self.SE_Punch = pygame.mixer.Sound('Resource\Sound\Effect\Punch.ogg')
+        #self.SE_Kick = pygame.mixer.Sound('Resource\Sound\Effect\Kick.mp3')
+        #self.SE_Kamehameha = pygame.mixer.Sound('Resource\Sound\Effect\Kamehameha.mp3')
+        #self.SE_PowerUp_Sound = pygame.mixer.Sound('Resource\Sound\Effect\PowerUp_Sound.mp3')
+        #self.SE_PowerUp = pygame.mixer.Sound('Resource\Sound\Effect\PowerUp.mp3')
+        #self.SE_PowerUp2 = pygame.mixer.Sound('Resource\Sound\Effect\PowerUp2.mp3')
+        #self.SE_Move = pygame.mixer.Sound('Resource\Sound\Effect\Move.mp3')
+        self.SE_Boss_Crashed = pygame.mixer.Sound('Resource\Sound\Effect\Boss_Crashed.ogg')
+        self.SE_Crash_Impact = pygame.mixer.Sound('Resource\Sound\Effect\Crash_Impact.ogg')
+        self.SE_Meteor_Crashed = pygame.mixer.Sound('Resource\Sound\Effect\Meteor_Crashed.ogg')
+
+
+
+
+
 
 
